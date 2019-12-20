d20f86117848f8476dbb939303233ca9459722cc
jigar.kapadia <jigar.kapadia@learningmate.com>
2019-12-20 18:55:01 +0530

    changed db configuration dynamic

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,10 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
 //remove before committing code
-const username = process.env.mongoUser || 'admin';
-const password = process.env.mongoPassword || 'admin123';
-const mongoDbUri = `mongodb://${username}:${password}@ds117535.mlab.com:17535/meandb`;
+const username = process.env.mongoUser;
+const password = process.env.mongoPassword;
+const rawUrl = process.env.mongoUrl;
+const mongoDbUri = rawUrl.replace('{username}', username).replace('{password}', password)
 mongoose.connect(mongoDbUri);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;

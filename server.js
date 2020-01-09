0fc7a2e3c5ad0e146b4700646d641f6cebbf5adc
jigar.kapadia <jigar.kapadia@learningmate.com>
2020-01-09 20:38:45 +0530

    changed port number

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const mongoose = require('./src/config/db.config');
 const morganLogger = require('morgan');
 const jwt = require('jsonwebtoken');
